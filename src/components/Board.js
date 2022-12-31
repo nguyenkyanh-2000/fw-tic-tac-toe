49157db5d1fb1833b2e5c54f5edab824550791e8
Anh Ky Nguyen <anh163@gmail.com>
2022-12-31 13:32:13 +0700

    First requirement

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,9 +5,48 @@ export default function Board({ squares, handleClick }) {
   return (
     <div className="board">
       <div>
-        <div className="board-row">"Your code here"</div>
-        <div className="board-row">"Your code here"</div>
-        <div className="board-row">"Your code here"</div>
+        <div className="board-row">
+          <Square
+            value={squares[0]}
+            handleClick={() => handleClick(0)}
+          ></Square>
+          <Square
+            value={squares[1]}
+            handleClick={() => handleClick(1)}
+          ></Square>
+          <Square
+            value={squares[2]}
+            handleClick={() => handleClick(2)}
+          ></Square>
+        </div>
+        <div className="board-row">
+          <Square
+            value={squares[3]}
+            handleClick={() => handleClick(3)}
+          ></Square>
+          <Square
+            value={squares[4]}
+            handleClick={() => handleClick(4)}
+          ></Square>
+          <Square
+            value={squares[5]}
+            handleClick={() => handleClick(5)}
+          ></Square>
+        </div>
+        <div className="board-row">
+          <Square
+            value={squares[6]}
+            handleClick={() => handleClick(6)}
+          ></Square>
+          <Square
+            value={squares[7]}
+            handleClick={() => handleClick(7)}
+          ></Square>
+          <Square
+            value={squares[8]}
+            handleClick={() => handleClick(8)}
+          ></Square>
+        </div>
       </div>
     </div>
   );
